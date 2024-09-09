832f20666da30170c27ee89cad1f3eafdbb65d16
Frosselly <11691962+Frosselly@users.noreply.github.com>
2024-09-09 18:06:38 +0300

    Removed dark mode

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,11 +1,28 @@
 import Input from "./Input";
 
-export default function GeneralInfo({props, onChange}) {
+export default function GeneralInfo({ props, onChange }) {
   return (
     <div>
-      <Input text={"Name:"} id={"name"} value={props.name} onChange={onChange}/>
-      <Input text={"Email:"} id={"email"} value={props.email} type={"email"}  onChange={onChange}/>
-      <Input text={"Phone number:"} id={"number"}  value={props.number} type={"number"}  onChange={onChange}/>
+      <Input
+        text={"Name:"}
+        id={"name"}
+        value={props.name}
+        onChange={onChange}
+      />
+      <Input
+        text={"Email:"}
+        id={"email"}
+        value={props.email}
+        type={"email"}
+        onChange={onChange}
+      />
+      <Input
+        text={"Phone number:"}
+        id={"number"}
+        value={props.number || ""}
+        type={"number"}
+        onChange={onChange}
+      />
     </div>
   );
 }
